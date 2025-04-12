3f67979a302de6894a1204afd20ee8307499d132
gitcommander78 <rawappeal@hotmail.com>
2025-04-12 19:44:06 +0200

    saving progress

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -10,15 +10,19 @@ import main.KeyHandler;
 public final class Player extends Entity {
     GamePanel gamePanel;
     KeyHandler keyHandler;
+    public final int screenX;
+    public final int screenY;
     public Player (GamePanel gamePanel, KeyHandler keyHandler) {
         this.gamePanel = gamePanel;
         this.keyHandler = keyHandler;
+        screenX = gamePanel.screenWidth/2 - (gamePanel.tileSize/2);
+        screenY = gamePanel.screenHeight/2 - (gamePanel.tileSize/2);
         setDefaultValues();
         getPlayerImage();
     }
     public void setDefaultValues() {
-        x = 100;
-        y = 100;
+        worldX = gamePanel.tileSize * 23;
+        worldY = gamePanel.tileSize * 21;
         speed = 4;
         direction = "down";
     }
@@ -41,19 +45,19 @@ public final class Player extends Entity {
                 keyHandler.leftPressed == true || keyHandler.rightPressed == true) {
             if (keyHandler.upPressed == true) {
                 direction = "up";
-                y -= speed;
+                worldY -= speed;
             }
             else if (keyHandler.downPressed == true) {
                 direction = "down";
-                y += speed;
+                worldY += speed;
             }
             else if (keyHandler.leftPressed == true) {
                 direction = "left";
-                x -= speed;
+                worldX -= speed;
             }
             else if (keyHandler.rightPressed == true) {
                 direction = "right";
-                x += speed;
+                worldX += speed;
             }
             spriteCounter++;
             if (spriteCounter > 12) {
@@ -110,7 +114,7 @@ public final class Player extends Entity {
             case "right" -> image = right1;
             */
         }
-        g2.drawImage(image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gamePanel.tileSize, gamePanel.tileSize, null);
         /* 
         Debug & Testing
         Rectangle sprite 
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -16,11 +16,15 @@ public class GamePanel extends JPanel implements Runnable {
     public final int maxScreenRow = 12;
     public final int screenWidth = tileSize * maxScreenCol;
     public final int screenHeight = tileSize * maxScreenRow;
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxScreenRow;
     int FPS = 60;
     TileManager tileManager = new TileManager(this);
     KeyHandler keyHandler = new KeyHandler();
     Thread gameThread;
-    Player player = new Player(this, keyHandler);
+    public Player player = new Player(this, keyHandler);
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
